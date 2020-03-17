26c6dd53dac087a6e82dd2276513371d97a44aa0
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-17 16:09:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# Your Code Here
+
+def map(array)
+    #map returns a new array of manipulated elements
+    new =[]
+    i = 0
+    while i < array.length
+      new.push(yield(array[i]))
+      i +=1
+    end
+    new
+  
+ 
